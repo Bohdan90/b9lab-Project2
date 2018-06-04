e46e56386ee79be6fb8dd8902cc47a88861d61bd
BSkrypka <bkrypka@luxoft.com>
2018-06-04 20:49:21 +0300

    Added receiver. Removed unused imports

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.4.4;
 
-import "./Ownable.sol";
-import "./ConvertShop.sol";
+
+
 import "./Destroyable.sol";
 import "./Stoppable.sol";
 
 contract Remittance is Stoppable, Destroyable {
 
   address private owner;
-
+  address private recipient;
 
   uint private finalDate;
   uint256 private currDate;
@@ -17,13 +17,15 @@ contract Remittance is Stoppable, Destroyable {
   event LogMoneySending(address to, uint amount);
   event LogReturnMoney(address to, uint amount);
 
-  constructor(uint daysAvailable,  bytes32 passwd)
+  constructor(uint daysAvailable,address receiver,  bytes32 passwd)
   public payable
   {
     require(daysAvailable != 0);
+    require(receiver != 0);
     currDate = now;
     owner = msg.sender;
     finalDate = currDate + daysAvailable;
+    recipient = receiver;
     //Probably i should  do something like: unicPassw = keccak256("pass1", "pass2").
     unicPassw = passwd;
 
@@ -39,7 +41,7 @@ contract Remittance is Stoppable, Destroyable {
   function reckoning(string firstPassw, string secondPassw) onlyIfRunning public returns (bool){
     if (keccak256(firstPassw, secondPassw) == unicPassw) {
       emit LogMoneySending(msg.sender, this.balance);
-      msg.sender.transfer(this.balance);
+      recipient.transfer(this.balance);
       return true;
     } else {
       return false;
