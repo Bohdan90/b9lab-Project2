abb84760da179e421b525b8e8643d695e2ddb272
Bohdan Skrypka <bskrypka@luxoft.com>
2018-06-15 19:00:02 +0300

    Fixed remarks

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -15,31 +15,33 @@ contract Remittance is Stoppable, Destroyable {
     uint balance;
   }
 
-  RemmitanceData tempData;
 
-  event LogNewRemittanceData(address sender, address recipien, bytes32 hashPass, uint deadline, uint balance);
-  event LogClaimRemmitance(address to, uint balance);
-  event LogCashBack(address to, uint balance);
+  event LogNewRemittanceData(address indexed sender, address recipien, bytes32 hashPass, uint deadline, uint balance);
+  event LogClaimRemmitance(address indexed to, uint balance);
+  event LogCashBack(address indexed to, uint balance);
 
 
   function setRemittanceData(uint daysAvailable, address receiver, bytes32 hashPass) public payable {
-    RemmitanceData tempRemitance;
+
     require(daysAvailable != 0);
     require(receiver != 0);
-    tempRemitance.owner = msg.sender;
-    tempRemitance.deadline = now + (daysAvailable * 86400);
-    tempRemitance.recipient = receiver;
-    tempRemitance.balance = msg.value;
-    remData[hashPass] = tempRemitance;
-    emit LogNewRemittanceData(tempRemitance.owner, tempRemitance.recipient, hashPass,tempRemitance.deadline, tempRemitance.balance);
+    require( msg.value != 0);
+    remData[hashPass] = RemmitanceData({
+      owner: msg.sender,
+      recipient: receiver,
+      deadline: now + (daysAvailable * 86400),
+      balance: msg.value
+      });
+    emit LogNewRemittanceData(msg.sender, receiver, hashPass,remData[hashPass].deadline, msg.value);
   }
 
 
   function claimRemittance(string firstPassw) onlyIfRunning public returns (bool){
-    tempData = remData[keccak256(msg.sender, firstPassw)];
+
+    RemmitanceData storage tempData = remData[hashHelper(firstPassw, msg.sender)];
     require(tempData.recipient != 0);
     require(tempData.balance != 0);
-    remData[keccak256(msg.sender, firstPassw)].balance = 0;
+    remData[hashHelper(firstPassw, msg.sender)].balance = 0;
     emit LogClaimRemmitance(tempData.recipient, tempData.balance);
     tempData.recipient.transfer(tempData.balance);
     return true;
@@ -48,18 +50,18 @@ contract Remittance is Stoppable, Destroyable {
 
   function cashBack(string firstPassw) onlyIfRunning public returns (bool success)
   {
-    tempData = remData[keccak256(msg.sender, firstPassw)];
+    RemmitanceData storage tempData = remData[hashHelper(firstPassw, msg.sender)];
     require(tempData.balance != 0);
     require(tempData.owner != 0);
     require(tempData.deadline < now);
-    remData[keccak256(msg.sender, firstPassw)].balance = 0;
+    remData[hashHelper(firstPassw, msg.sender)].balance = 0;
     emit LogCashBack(tempData.owner, tempData.balance);
     tempData.owner.transfer(tempData.balance);
     return true;
   }
 
-  function hashHelper(string passw) public pure returns (bytes32 hash) {
-    return keccak256(msg.sender, passw);
+  function hashHelper(string passw, address userAddr) public pure returns (bytes32 hash) {
+    return keccak256(userAddr, passw);
   }
 
 }
